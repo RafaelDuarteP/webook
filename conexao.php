ea17dbd71424f5d135f2f82d0109836843149502
Rafael Duarte Pereira <72765074+RafaelDuarteP@users.noreply.github.com>
2021-06-22 11:12:00 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,10 +2,11 @@
 
 /* Classe de conexão ao banco de dados */
 
-    $host = "localhost";
-    $user = "root";
-    $senha = "";
-    $banco = "webook";
+    $host = "fdb33.awardspace.net";
+    $user = "3878141_webook";
+    $senha = "webooktis2021";
+    $banco = "3878141_webook";
     $conexao = mysqli_connect($host,$user,$senha,$banco);
+
 
 ?>
